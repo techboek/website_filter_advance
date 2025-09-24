337807d3f3c3f57b8d5830e4b162865ca8932bf5
techboek <techboek9@gmail.com>
2025-09-24 15:36:31 +0000

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
     'version': '16.0.1.0',
     'author': 'BOEK Tech',
     'description': """This module enables the creation and management of advanced product filters in Odoo, providing customers with an enhanced browsing experience on the website. """,
-    'summary': 'Advanced Product Filters by Category.',
+    'summary': 'Advanced Product Filters ',
     'depends': ['website_sale', 'sale_management'],
     'license': 'LGPL-3',
     'price': 100,
